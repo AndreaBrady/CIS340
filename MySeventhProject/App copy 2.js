2569547c2be17af4cb7ea96cb4369bdd8e6fa169
AndreaBrady <63574023+AndreaBrady@users.noreply.github.com>
2020-05-08 01:04:34 +0000

    Adding Buttons And Upload Images

--- MySeventhProject/App copy 2.js
+++ MySeventhProject/App copy 2.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+export default class TouchableButton extends Component {
+
+  onPressButton(){
+    alert('You tapped a button.')
+  }
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.containerButton}>
+        <Button
+        onPress={this.onPressButton}
+          title="Press Button 1"
+      />
+       </View>
+
+        <View style={styles.containerButtonw}>
+
+       <Button
+      onPress={this.onPressButton}
+         title="Press Button 2"
+     />
+ <Button
+        onPress={this.onPressButton}
+          title="Great!"
+          color = 'green'
+      />
+        
+        </View>
+
+     <View style={{styles.containerButton}}>
+     <Button
+        onPress={() => {
+          alert('You tapped button Tap Me!'):
+        }}
+          title="Tap Me!"
+      />
+
+
+     </View>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerButton: {
+    margin: 20,
+  },
+
+  containerLayoutButton: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+
 });
